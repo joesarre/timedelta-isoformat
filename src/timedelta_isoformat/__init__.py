00d17270f6ad4528c0d091f835282a4c803ae7b7
James Addison <jay@jp-hosting.net>
2023-03-17 11:11:13 +0000

    Refactor: simplify the character-in-context conditional check

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -156,14 +156,13 @@ class timedelta(datetime.timedelta):
                 pass
 
             assert not (unit and context is week_context), "cannot mix weeks with other units"
-            for delimiter in context:
-                if char == delimiter:
-                    unit = char
-                    yield (TimeComponent if context is time_context else DateComponent)(head + tail, unit)
-                    head = tail = ""
-                    break
-            else:
+
+            # Note: this advances and may exhaust the context iterator
+            if char not in context:
                 raise ValueError(f"unexpected character '{char}'")
+
+            yield (TimeComponent if context is time_context else DateComponent)(head + tail, char)
+            head, tail, unit = "", "", char
 
         assert unit, "no measurements found"
 
